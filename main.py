eeab39f0da64534f401f8ead7b4d78a1688fb83f
Le Hong Phuc <lehongphucnasa@gmail.com>
2026-06-02 16:35:39 +0700

    Initial commit, this version use openAI

---
+++ main.py
@@ -0,0 +1,46 @@
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
+from inngest.experimental import ai
+from data_loader import load_and_chunk_pdf, embed_texts
+from vector_db import QdrantStorage
+from custom_type import RAGQueryResult, RAGChunkAndSrc, RAGUpsertResult, RAGSearchResult
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="rag-production-app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAG: Ingest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+    def _load(ctx: inngest.Context) -> RAGChunkAndSrc:
+        pdf_path = ctx.event.data["pdf_path"]
+        source_id = ctx.event.data.get("source_id", pdf_path)
+        chunks = load_and_chunk_pdf(pdf_path)
+        return RAGChunkAndSrc(chunks=chunks, source_id=source_id)
+    def _upsert(chunk_and_src: RAGChunkAndSrc) -> RAGUpsertResult:
+        chunks = chunk_and_src.chunks
+        source_id = chunk_and_src.source_id
+        vecs = embed_texts(chunks)
+        ids = [str(uuid.uuid5(uuid.NAMESPACE_URL, f"{source_id}_{i}")) for i in range(len(chunks))]
+        payloads = [{"source": source_id, "text": chunks[i]} for i in range(len(chunks))]
+        QdrantStorage().upsert(ids, vecs, payloads)
+        return RAGUpsertResult(ingested=len(chunks))
+    chunks_and_src = await ctx.step.run("load_and_chunk_pdf", lambda: _load(ctx), output_type = RAGChunkAndSrc)
+    inngested = await ctx.step.run("embed_and_upsert", lambda: _upsert(chunks_and_src), output_type = RAGUpsertResult)
+    return inngested.model_dump()
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])
